1d1cdaac78c72714d158a9b114b7036fa27a92ed
cjahn1305 <152269511+cjahn1305@users.noreply.github.com>
2024-07-12 14:16:16 +0200

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,17 @@
-import { Router } from "express";
-import TodosRouter from "./todos";
+const { Router } = require("express");
+const { TodosRouter } = require("./todos");
+const { AuthRouter } = require("./auth");
+const { UserRouter } = require("./users");
+const logger = require("../services/logger/Logger");
 
 const AppRouter = Router();
 
+AppRouter.use("/auth", AuthRouter);
+AppRouter.use("/users", authMiddleWare, UserRouter);
 AppRouter.use("/todos", TodosRouter);
-
-AppRouter.get('/', (req, res) => {
-  res.status(200).send('Hello, World!');
+AppRouter.get("/test", (req, res) => {
+  console.log("Hello World");
+  logger.error("Hello World");
+  res.json({ myLogLevel: process.env.LOG_LEVEL });
 });
-
-export default AppRouter;
+module.exports = { AppRouter };
